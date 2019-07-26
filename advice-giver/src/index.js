141bb0fb036533a7617506a1ff8e443aa557587d
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2019-07-25 19:18:13 -0500

    Basic Redux Boilerplate Setup

--- advice-giver/src/index.js
+++ advice-giver/src/index.js
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom'
+import thunk from 'redux-thunk'
+// import  reducer  from './Reducer'
+import logger from 'redux-logger'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const logger = (store) => (next) => (action) =>{
+//     next(action)
+// }
+
+const store = createStore(
+    // reducer,
+    applyMiddleware(thunk, logger),
+)
+ReactDOM.render(
+<Provider store={store}>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+</Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
