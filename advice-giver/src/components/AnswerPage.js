a13d8c78569e9a864174ce53fe222eae9ffceb87
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2019-07-31 14:45:21 -0500

    work work work

--- advice-giver/src/components/AnswerPage.js
+++ advice-giver/src/components/AnswerPage.js
@@ -1,49 +1,156 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+// import React, { Component } from 'react';
+// import '../App.css';
+// import Users from './Users';
+// import btn_icon_back_advicegiver from './images/btn_icon_back_advicegiver.png';
 
-export default class Create extends Component {
+// // UI framework component imports
+// import Appbar from 'muicss/lib/react/appbar';
 
-    constructor(props) {
-        super(props);
-        this.onChangeHostName = this.onChangeHostName.bind(this);
-        this.onChangePort = this.onChangePort.bind(this);
-        this.onChangeEmail = this.onChangePort.bind(this);
-        this.gimme = this.gimme.bind(this);
 
-        this.state = {
-        username: '',
-        password: '',
-        }
-    }
-    onChangeHostName(e) {
-        this.setState({
-            name: e.target.value
-        });
-    }
-    onChangePort(e) {
-        this.setState({
-            password: e.target.value
-        });
-    }
+// export default class ADVICEGIVERScreen extends Component {
+
+//   // Properties used by this component:
+//   // appActions, deviceInfo
+
+//   onClick_elSwapper = (ev) => {
+//     // Go back in screen navigation history
+//     this.props.appActions.goBack();
+  
+//   }
+  
+  
+//   render() {
+//     // eslint-disable-next-line no-unused-vars
+//     let baseStyle = {};
+//     // eslint-disable-next-line no-unused-vars
+//     let layoutFlowStyle = {};
+//     if (this.props.transitionId && this.props.transitionId.length > 0 && this.props.atTopOfScreenStack && this.props.transitionForward) {
+//       baseStyle.animation = '0.25s ease-in-out '+this.props.transitionId;
+//     }
+//     if ( !this.props.atTopOfScreenStack) {
+//       layoutFlowStyle.height = '100vh';
+//       layoutFlowStyle.overflow = 'hidden';
+//     }
     
-    gimme(e) {
-        e.preventDefault();
-        axios.get('https://advice-giver.herokuapp.com/messages', {
-            headers: { Authorization: localStorage.getItem('token') }})
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-    }
+//     const dataSheet_sheet1 = this.props.dataSheets['sheet1'];
+//     const style_elBackground = {
+//         width: '100%',
+//         height: '100%',
+//      };
+//     const style_elBackground_outer = {
+//         backgroundColor: '#f6f6f6',
+//      };
+    
+//     let contentElement_elSwapper;  // This element's content can vary based on screen size
+    
+//     const style_elSwapper_outer = {
+//         cursor: 'pointer',
+//         pointerEvents: 'auto',
+//      };
+//     const style_elList = {
+//         height: 'auto',  // This element is in scroll flow
+//      };
+//     // Source items and any special components used for list/grid element 'list'.
+//     let items_list = [];
+//     let listComps_list = {};
+//     items_list = items_list.concat(this.props.appActions.getDataSheet('sheet1').items);
+    
+    
+//     return (
+//       <div className="AppScreen ADVICEGIVERScreen" style={baseStyle}>
+//         <div className="background">
+//           <div className='containerMinHeight elBackground' style={style_elBackground_outer}>
+//             <div className='appBg' style={style_elBackground} />
+          
+//           </div>
+          
+//         </div>
+//         <div className="layoutFlow" style={layoutFlowStyle}>
+//           <div className="flowRow flowRow_ADVICEGIVERScreen_elSwapper_872343">
+//           <div className='hasNestedComps elSwapper' style={style_elSwapper_outer}>
+//             <div className='' onClick={this.onClick_elSwapper} >
+//               {contentElement_elSwapper}
+//             </div>
+          
+//           </div>
+          
+//           <div className='hasNestedComps elList'>
+//             <div className='' style={style_elList}>
+//               {items_list.map((row, index) => {
+//                 let itemClasses = `gridItem cols2_${index % 2}`;
+//                 let itemComp = (row._componentId) ? listComps_list[row._componentId] : <Users dataSheetId={'sheet1'} dataSheetRow={row} {...{ 'name': row['name'], }} appActions={this.props.appActions} deviceInfo={this.props.deviceInfo} locStrings={this.props.locStrings} />;
+//                 return (
+//                   <div className={itemClasses} key={row.key}>
+//                     {itemComp}
+//                   </div>
+//                 )
+//               })}
+//               <div ref={(el)=> this._elList_endMarker = el} />
+//             </div>
+          
+//           </div>
+          
+//           </div>
+//         </div>
+//         <Appbar className="navBar">
+//           <div className="title">ADVICE GIVER</div>  <div className="backBtn" onClick={ (ev)=>{ this.props.appActions.goBack() } }><img src={btn_icon_back_advicegiver} alt="" style={{width: '50%'}} /></div>
+//         </Appbar>
+        
+//       </div>
+//     )
+//   }
+  
 
-    render() {
-        return (
-            <div>
-                <h3>Display?</h3>
-               <button onClick={this.gimme}>try this I guess</button>
-            </div>
-        )
-    }
-}
+// }
+
+
+
+// import React, { Component } from 'react';
+// import axios from 'axios';
+
+// export default class Create extends Component {
+
+//     constructor(props) {
+//         super(props);
+//         this.onChangeHostName = this.onChangeHostName.bind(this);
+//         this.onChangePort = this.onChangePort.bind(this);
+//         this.onChangeEmail = this.onChangePort.bind(this);
+//         this.gimme = this.gimme.bind(this);
+
+//         this.state = {
+//         username: '',
+//         password: '',
+//         }
+//     }
+//     onChangeHostName(e) {
+//         this.setState({
+//             name: e.target.value
+//         });
+//     }
+//     onChangePort(e) {
+//         this.setState({
+//             password: e.target.value
+//         });
+//     }
+    
+//     gimme(e) {
+//         e.preventDefault();
+//         axios.get('https://advice-giver.herokuapp.com/messages', {
+//             headers: { Authorization: localStorage.getItem('token') }})
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h3>Display?</h3>
+//                <button onClick={this.gimme}>try this I guess</button>
+//             </div>
+//         )
+//     }
+// }
--- advice-giver/src/components/SignUp.js
+++ advice-giver/src/components/SignUp.js
@@ -1,189 +1,83 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux'
+import { newUser } from './actions'
 
-export default class Create extends Component {
-
-    constructor(props) {
-        super(props);
-        this.onChangeHostName = this.onChangeHostName.bind(this);
-        this.onChangePort = this.onChangePort.bind(this);
-        this.onChangeEmail = this.onChangeEmail.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-
-        this.state = {
-            name: '',
-            password: '',
-            email: '',
-            adviceGiver: false,
-            expertise: null,
-            yearsOfExperience: 10,
-            age: 30,
-        }
+class SignUp extends React.Component {
+    state = {
+      credentials: {
+        username: '',
+        password: '',
+        email: '',
+      }
     }
-    onChangeHostName(e) {
-        this.setState({
-            name: e.target.value
-        });
-    }
-    onChangePort(e) {
-      this.setState({
-          password: e.target.value
-      });
-  }
-  onChangeEmail(e) {
-    this.setState({
-        email: e.target.value
-    });
-}
-    onSubmit(e) {
-        e.preventDefault();
-        const user = {
-          name: this.state.name,
-          password: this.state.password,
-          email: this.state.email,
-          adviceGiver: false,
-        } = this.state
-
-        console.log(this.state)
-
-       
-        axios.post('https://advice-giver.herokuapp.com/auth/register', user)
-      .then(function (response) {
-          console.log(response);
-      })
-      .catch(function (error) {
-          console.log(error);
-      });
-        
-        
-        
-        // this.setState({
-        //     name: '',
-        //     password: '',
-        //     email:'',
-        //     adviceGiver: false,
-        //     expertise: null,
-        //     yearsOfExperience: 10,
-        //     age: 30,
-        // });
-    }
-
-    render() {
-        return (
-            <div style={{marginTop: 50}}>
-                <h3>Add New User</h3>
-                <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label>Name:  </label>
-                        <input type="text" className="form-control" onChange={this.onChangeHostName}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Password: </label>
-                        <input type="text" className="form-control"onChange={this.onChangePort}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Email: </label>
-                        <input type="text" className="form-control"onChange={this.onChangeEmail}/>
-                    </div>
-                    <div className="form-group">
-                        <input type="submit" value="Add Node server" className="btn btn-primary"/>
-                    </div>
-                </form>
-            </div>
-        )
-    }
-}
-
-// import React, { Component } from 'react';
-// import { connect } from 'react-redux'
-// import { newUser } from './actions'
-
-// class SignUp extends React.Component {
-//     constructor(){
-//         super()
-//     this.state = {
-//       credentials: {
-//         username: '',
-//         password: '',
-//         email: '',
-//       }
-//     }
     
   
-//     textInputChanged_field = (event) => {
-//       this.setState({username: event.target.value});
-//     }
+    textInputChanged_field = (event) => {
+      this.setState({username: event.target.value});
+    }
     
-//     textInputChanged_field2 = (event) => {
-//         this.setState({password: event.target.value});
-//       }
+    textInputChanged_field2 = (event) => {
+        this.setState({password: event.target.value});
+      }
 
-//     textInputChanged_field3 = (event) => {
-//         this.setState({email: event.target.value});
-//       }  
+    textInputChanged_field3 = (event) => {
+        this.setState({email: event.target.value});
+      }  
     
-//     handleSignup = (e) => {
-//         e.preventDefault();
-//         let state = {
-//             credentials: {
-//               username: '',
-//               password: '',
-//               email: '',
-//             }
-//           }
-//         const { username, password, email } = this.state
-//         console.log(this.state);
-//         this.props
-//           .newUser(this.state)
-//           // .then(() => this.props.history.push("/auth"));
+    handleSignup = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+        // this.props
+        //   .newUser(this.state.credentials)
+        //   .then(() => this.props.history.push("/auth"));
     
-//     }
-
+    }
     
-//     render() {
+    
+    render() {
       
       
-//       return (
-//         <div>
-//           <form onSubmit={this.handleSignup}>
+      return (
+        <div>
+          <form onSubmit={this.handleSignup}>
           
             
-//             <div>
-//               <input className='baseFont' type="text" placeholder='Username' onChange={this.textInputChanged_field} value={this.state.username}  />
+            <div>
+              <input className='baseFont' type="text" placeholder='Username' onChange={this.textInputChanged_field} value={this.state.username}  />
             
-//             </div>
+            </div>
             
-//             <div className='elField2'>
-//               <input className='baseFont'type="text" placeholder='Password' onChange={this.textInputChanged_field2} value={this.state.password}  />
+            <div className='elField2'>
+              <input className='baseFont'type="text" placeholder='Password' onChange={this.textInputChanged_field2} value={this.state.password}  />
             
-//             </div>
+            </div>
 
-//             <div className='elField2'>
-//               <input className='baseFont'type="text" placeholder='Email' onChange={this.textInputChanged_field3} value={this.state.email}  />
+            <div className='elField2'>
+              <input className='baseFont'type="text" placeholder='Email' onChange={this.textInputChanged_field3} value={this.state.email}  />
             
-//             </div>
+            </div>
             
             
-//             <div>
-//               <button>Register</button>
+            <div>
+              <button>Register</button>
             
-//             </div>
+            </div>
             
           
          
-//             </form>
-//         </div>
-//       )
-//     }
+            </form>
+        </div>
+      )
+    }
     
   
-//   }}
+  }
 
-//   const mapStateToProps = state =>{
-//     return{
-//         smurfs: state.smurfs
-//     }
-// }
+  const mapStateToProps = state =>{
+    return{
+        smurfs: state.smurfs
+    }
+}
 
-//   export default connect(
-//     mapStateToProps, { newUser })(SignUp)
+  export default connect(
+    mapStateToProps, { newUser })(SignUp)
