4859d38c777d6f1aced1ddbe008f969df34102ea
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2019-07-30 19:40:55 -0500

    Importing the login screen

--- advice-giver/src/App.js
+++ advice-giver/src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
 import React from 'react'
 import { Route } from 'react-router-dom'
+import Login from './components/Login'
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>App.js Text</h1>
+        <Login />
       </div>
     )
   }
---
+++ advice-giver/src/components/Login.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+
+export default class LoginScreen extends Component {
+
+    // Properties used by this component:
+    // appActions, deviceInfo
+  
+    constructor(props) {
+      super(props);
+      
+      this.state = {
+        field: '',
+        field2: '',
+      };
+    }
+  
+    textInputChanged_field = (event) => {
+      this.setState({field: event.target.value});
+    }
+    
+    textInputChanged_field2 = (event) => {
+      this.setState({field2: event.target.value});
+    }
+    
+    onClick_elButton = (ev) => {
+      // Go to screen 'ADVICE GIVER'
+      this.props.appActions.goToScreen('advicegiver', { transitionId: 'fadeIn' });
+    
+    }
+    
+    
+    render() {
+      
+      
+      return (
+        <div className="AppScreen LoginScreen" >
+          <div className="background">
+            <div className='containerMinHeight elBackground'>
+              <div className='appBg'/>
+            
+            </div>
+            
+          </div>
+          <div className="layoutFlow" >
+            <div className='elText'>
+              <div className='baseFont' >
+                <div>Username</div>
+              </div>
+            
+            </div>
+            
+            <div className='elText2'>
+              <div className='baseFont'>
+                <div>Password</div>
+              </div>
+            
+            </div>
+            
+            <div className='elField'>
+              <input className='baseFont' type="text" placeholder='Username' onChange={this.textInputChanged_field} value={this.state.field}  />
+            
+            </div>
+            
+            <div className='elField2'>
+              <input className='baseFont'type="text" placeholder='Password' onChange={this.textInputChanged_field2} value={this.state.field2}  />
+            
+            </div>
+            
+            <div className='elButton'>
+              <button className='actionFont' color="accent" onClick={this.onClick_elButton} >Login
+              </button>
+            
+            </div>
+            
+          </div>
+         
+          
+        </div>
+      )
+    }
+    
+  
+  }
+  
+  
