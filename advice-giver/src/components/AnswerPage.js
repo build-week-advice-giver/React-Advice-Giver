1f85fc4bb140058742effd88dcb1578e84886a7a
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2019-08-01 15:32:42 -0500

    Messages can be displayed in answer

--- advice-giver/src/components/AnswerPage.js
+++ advice-giver/src/components/AnswerPage.js
@@ -8,19 +8,19 @@ export default class Create extends Component {
         super(props);
         this.gimme = this.gimme.bind(this);
 
-        this.state = {
-        messages: '',
-        }
+        this.state = {messages: []};
+    
     }
     
     gimme(e) {
         e.preventDefault();
         axios.get('https://advice-giver.herokuapp.com/messages', {
             headers: { Authorization: localStorage.getItem('token') }})
-  .then(function (response) {
-    console.log(response.data);
-   }
-)
+            .then(response => {
+                this.setState({ messages: response.data });
+              })
+   
+
   .catch(function (error) {
     console.log(error);
   })}
@@ -30,7 +30,11 @@ export default class Create extends Component {
         return (
             <div>
                 <h3>Display?</h3>
-               <button onClick={this.gimme}>try this I guess</button>
+               <button onClick={this.gimme}>Load Messages</button>
+               <p>{this.state.messages.map((item, index) => (
+  <p key={index}>{item.message}</p>
+))}</p>
+               
 
                
             </div>
