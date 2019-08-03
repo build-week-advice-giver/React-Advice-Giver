4df44c784a6ae836d035c162be22f7d6b562698d
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2019-08-02 21:32:58 -0500

    Updates

---
+++ precache-manifest.9f6d9352173eb3bd44c177eebec52cd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64c9e8d6b0527786593f02c351bae5c",
+    "url": "/React-Advice-Giver/index.html"
+  },
+  {
+    "revision": "54e9b6ac501b61fad975",
+    "url": "/React-Advice-Giver/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "60399f6e7278f13eb572",
+    "url": "/React-Advice-Giver/static/js/2.ef3b5880.chunk.js"
+  },
+  {
+    "revision": "54e9b6ac501b61fad975",
+    "url": "/React-Advice-Giver/static/js/main.232140c4.chunk.js"
+  },
+  {
+    "revision": "f791fc91751e71ab1e7c",
+    "url": "/React-Advice-Giver/static/js/runtime~main.aa858a9d.js"
+  }
+]);
